56c84cc96f595fcb727756959134a8ddb7d14ba8
rahimasalman <rehimesalman7@gmail.com>
2024-03-30 15:14:59 +0400

    feat: add tailwind config and use classes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <nuxt-link to="/">Nuxt App Demo</nuxt-link>
-                <ul>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4 flex justify-between">
+                <nuxt-link to="/" class="font-bold">Nuxt App Demo</nuxt-link>
+                <ul class="flex gap-4">
                     <li><nuxt-link to="/">Home</nuxt-link></li>
                     <li><nuxt-link to="/about">About</nuxt-link></li>
-                    <li><nuxt-link to="/products">Products</nuxt-link></li>
+                    <li><nuxt-link to="/products" class="btn">Products</nuxt-link></li>
                 </ul>
             </nav>
         </header>
         <!-- output the page content -->
-        <div>
+        <div class="container mx-auto p-4">
             <slot />
         </div>
     </div>
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <nuxt-link to="/products">Nuxt App Products</nuxt-link>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4">
+                <nuxt-link to="/products" class="font-bold">Nuxt App Products</nuxt-link>
             </nav>
         </header>
         <!-- output the page content -->
-        <div>
+        <div class="container mx-auto p-4">
             <slot />
         </div>
-        <footer>
-            <ul>
+        <footer class="container mx-auto p-4 flex justify-between border-t-2">
+            <ul class="flex gap-4">
                 <li><nuxt-link to="/">Home</nuxt-link></li>
                 <li><nuxt-link to="/about">About</nuxt-link></li>
                 <li><nuxt-link to="/products">Products</nuxt-link></li>
