f964b84b4c721cf55b37d39f341b17eb1babc72e
rahimasalman <rehimesalman7@gmail.com>
2024-04-01 17:34:29 +0400

    feat: add server routes for front-end

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,22 @@
 <template>
-    <div>
-        <h1>
-            About
-        </h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid veniam facilis molestiae? Non, sapiente?
-            Deserunt sed eligendi, repellat, aliquid amet?
-        </p>
-
-    </div>
+  <div>
+    <h1>
+      About
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid veniam facilis molestiae? Non, sapiente?
+      Deserunt sed eligendi, repellat, aliquid amet?
+    </p>
+    <div>{{ data }}</div>
+    <div>{{ postData }}</div>
+  </div>
 </template>
 
 <script setup>
-
+const {data} = await useFetch('/api/test?name=mario', {
+  method: 'post',
+  body: {age: 40}
+})
 </script>
 
 <style scoped></style>
