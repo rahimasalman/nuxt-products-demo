fcfd1ccaa5408e336397fb6679647edc6f06d7d5
rahimasalman <rehimesalman7@gmail.com>
2024-04-01 15:43:38 +0400

    feat: add error page

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,19 +1,23 @@
 <template>
-    <div>
-        <ProductDetails :product="product" />
-    </div>
+  <div>
+    <ProductDetails :product="product"/>
+  </div>
 </template>
 
 <script setup>
-const { id } = useRoute().params
+const {id} = useRoute().params
 const uri = 'https://fakestoreapi.com/products/' + id
 
 
 //  fetch the single product
-const { data: product } = await useFetch(uri, { key: id })
+const {data: product} = await useFetch(uri, {key: id})
+
+if (!product.value) {
+  throw createError({statusCode: 404, statusMessage: 'Page not found', fatal: true})
+}
 
 definePageMeta({
-    layout: 'products'
+  layout: 'products'
 })
 </script>
 
